955c8d87985dc9d5ff26bf1d734cc9237985fe72
tudb.aivy@gmail.com <tudb.aivy@gmail.com>
2023-05-23 14:50:15 +0700

    update

--- main.js
+++ main.js
@@ -213,7 +213,46 @@ function hideChat(hide) {
       <div class="chat_body chat_login">
           <a id="chat_first_screen" class="fab"><i class="zmdi zmdi-arrow-right"></i></a>
           <p>We make it simple and seamless for businesses and people to talk to each other. Ask us anything</p>
-      </div>
+          <form>
+          <div class="form__field">
+              <label for="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Enter your name"
+              />
+          </div>
+          <div class="form__field">
+              <label for="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Enter your email"
+              />
+          </div>
+          <div class="form__field">
+              <label for="subject">Subject</label>
+              <input
+                type="text"
+                id="subject"
+                name="subject"
+                placeholder="Enter Message Subject"
+              />
+          </div>
+          <div class="form__field">
+              <label for="message">Message</label>
+              <textarea
+                id="message"
+                name="message"
+                placeholder="Enter your message"
+                rows="6"
+              ></textarea>
+          </div>
+          <button>Send Message</button>
+      </form>
+          </div>
       <div id="chat_converse" class="chat_conversion chat_converse">
               <a id="chat_second_screen" class="fab"><i class="zmdi zmdi-arrow-right"></i></a>
         <span class="chat_msg_item chat_msg_item_admin">
